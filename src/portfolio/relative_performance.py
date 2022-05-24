004755bb71795b9898701fd444496239bb5c7233
Moritz Wilksch <moritzwilksch@gmail.com>
2022-05-24 08:04:17 +0000

    add relative performance

---
+++ src/portfolio/relative_performance.py
@@ -0,0 +1,44 @@
+#%%
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('pdf', 'svg')
+
+#%%
+tickers = [
+    "JEPI",
+    "SCHD",
+    "SPYD",
+    "DIVO",
+    "SPHD",
+    "DGRO"
+    # "VYM",
+    # "VIG"
+]
+data = yf.Tickers(tickers).history("5y")[["Dividends", "Close"]]
+close, divis = data["Close"], data["Dividends"]
+
+#%%
+missing = close.isna().any(axis=1)
+close, divis = close[~missing], divis[~missing]
+
+#%%
+close_divs_reinvested = close + divis.cumsum()
+close = close / close.iloc[0]
+close_divs_reinvested = close_divs_reinvested / close_divs_reinvested.iloc[0]
+
+#%%
+plot_close = close.reset_index().melt(var_name="ticker", id_vars="Date")
+plot_close_divs_reinvested = close_divs_reinvested.reset_index().melt(var_name="ticker", id_vars="Date")
+
+#%%
+fig, ax = plt.subplots(figsize=(15, 8))
+sns.lineplot(data=plot_close, y="value", hue="ticker", x="Date", ax=ax)
+sns.despine()
+
+#%%
+fig, ax = plt.subplots(figsize=(15, 8))
+sns.lineplot(data=plot_close_divs_reinvested, y="value", hue="ticker", x="Date", ax=ax)
+sns.despine()
---
+++ src/utils/data.py
@@ -0,0 +1,6 @@
+#%%
+import yfinance as yf
+
+t = yf.Ticker('TSLA')
+
+#%%
